124626008ff8d00e1aad20a1626125f96b6fe63c
mateusmoov <mateusvasconcelos4210@gmail.com>
2024-03-07 17:59:14 -0300

    feat: add array of card objects

---
+++ src/utils/cards.ts
@@ -0,0 +1,249 @@
+type TrucoCard = {
+  imageUrl: string;
+  manilha: string;
+  weight: number;
+  name: string;
+};
+
+export const cards: TrucoCard[] = [
+  { 
+    imageUrl: "public/assets/cards/ace_of_coins.png", 
+    manilha: "2", 
+    weight: 1, 
+    name: "Ás de Ouros" 
+  },
+  { 
+    imageUrl: "public/assets/cards/2_of_coins.png", 
+    manilha: "3", 
+    weight: 2, 
+    name: "2 de Ouros" 
+  },
+  { 
+    imageUrl: "public/assets/cards/3_of_coins.png", 
+    manilha: "4", 
+    weight: 3, 
+    name: "3 de Ouros" 
+  },
+  { 
+    imageUrl: "public/assets/cards/4_of_coins.png", 
+    manilha: "5", 
+    weight: 4, 
+    name: "4 de Ouros" 
+  },
+  { 
+    imageUrl: "public/assets/cards/5_of_coins.png", 
+    manilha: "6", 
+    weight: 1, 
+    name: "5 de Ouros" 
+  },
+  { 
+    imageUrl: "public/assets/cards/6_of_coins.png", 
+    manilha: "7", 
+    weight: 2, 
+    name: "6 de Ouros" 
+  },
+  { 
+    imageUrl: "public/assets/cards/7_of_coins.png", 
+    manilha: "Q", 
+    weight: 3, 
+    name: "7 de Ouros" 
+  },
+  { 
+    imageUrl: "public/assets/cards/jack_of_coins.png", 
+    manilha: "K", 
+    weight: 4, 
+    name: "Valete de Ouros" 
+  },
+  { 
+    imageUrl: "public/assets/cards/queen_of_coins.png", 
+    manilha: "A", 
+    weight: 1, 
+    name: "Dama de Ouros" 
+  },
+  { 
+    imageUrl: "public/assets/cards/king_of_coins.png", 
+    manilha: "7", 
+    weight: 2, 
+    name: "Rei de Ouros" 
+  },
+  { 
+    imageUrl: "public/assets/cards/ace_of_spades.png", 
+    manilha: "2", 
+    weight: 1, 
+    name: "Ás de Espadas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/2_of_spades.png", 
+    manilha: "3", 
+    weight: 2, 
+    name: "2 de Espadas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/3_of_spades.png", 
+    manilha: "4", 
+    weight: 3, 
+    name: "3 de Espadas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/4_of_spades.png", 
+    manilha: "5", 
+    weight: 4, 
+    name: "4 de Espadas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/5_of_spades.png", 
+    manilha: "6", 
+    weight: 1, 
+    name: "5 de Espadas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/6_of_spades.png", 
+    manilha: "7", 
+    weight: 2, 
+    name: "6 de Espadas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/7_of_spades.png", 
+    manilha: "Q", 
+    weight: 3, 
+    name: "7 de Espadas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/jack_of_spades.png", 
+    manilha: "K", 
+    weight: 4, 
+    name: "Valete de Espadas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/queen_of_spades.png", 
+    manilha: "A", 
+    weight: 1, 
+    name: "Dama de Espadas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/king_of_spades.png", 
+    manilha: "7", 
+    weight: 2, 
+    name: "Rei de Espadas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/ace_of_hearts.png", 
+    manilha: "2", 
+    weight: 1, 
+    name: "Ás de Copas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/2_of_hearts.png", 
+    manilha: "3", 
+    weight: 2, 
+    name: "2 de Copas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/3_of_hearts.png", 
+    manilha: "4", 
+    weight: 3, 
+    name: "3 de Copas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/4_of_hearts.png", 
+    manilha: "5", 
+    weight: 4, 
+    name: "4 de Copas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/5_of_hearts.png", 
+    manilha: "6", 
+    weight: 1, 
+    name: "5 de Copas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/6_of_hearts.png", 
+    manilha: "7", 
+    weight: 2, 
+    name: "6 de Copas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/7_of_hearts.png", 
+    manilha: "Q", 
+    weight: 3, 
+    name: "7 de Copas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/jack_of_hearts.png", 
+    manilha: "K", 
+    weight: 4, 
+    name: "Valete de Copas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/queen_of_hearts.png", 
+    manilha: "A", 
+    weight: 1, 
+    name: "Dama de Copas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/king_of_hearts.png", 
+    manilha: "7", 
+    weight: 2, 
+    name: "Rei de Copas" 
+  },
+  { 
+    imageUrl: "public/assets/cards/ace_of_clubs.png", 
+    manilha: "2", 
+    weight: 1, 
+    name: "Ás de Paus" 
+  },
+  { 
+    imageUrl: "public/assets/cards/2_of_clubs.png", 
+    manilha: "3", 
+    weight: 2, 
+    name: "2 de Paus" 
+  },
+  { 
+    imageUrl: "public/assets/cards/3_of_clubs.png", 
+    manilha: "4", 
+    weight: 3, 
+    name: "3 de Paus" 
+  },
+  { 
+    imageUrl: "public/assets/cards/4_of_clubs.png", 
+    manilha: "5", 
+    weight: 4, 
+    name: "4 de Paus" 
+  },
+  { 
+    imageUrl: "public/assets/cards/5_of_clubs.png", 
+    manilha: "6", 
+    weight: 1, 
+    name: "5 de Paus" 
+  },
+  { 
+    imageUrl: "public/assets/cards/6_of_clubs.png", 
+    manilha: "7", 
+    weight: 2, 
+    name: "6 de Paus" 
+  },
+  { 
+    imageUrl: "public/assets/cards/7_of_clubs.png", 
+    manilha: "Q", 
+    weight: 3, 
+    name: "7 de Paus" 
+  },
+  { 
+    imageUrl: "public/assets/cards/jack_of_clubs.png", 
+    manilha: "K", 
+    weight: 4, 
+    name: "Valete de Paus" 
+  },
+  { 
+    imageUrl: "public/assets/cards/queen_of_clubs.png", 
+    manilha: "A", 
+    weight: 1, 
+    name: "Dama de Paus" 
+  },
+  { 
+    imageUrl: "public/assets/cards/king_of_clubs.png", 
+    manilha: "7", 
+    weight: 2, 
+    name: "Rei de Paus" 
+  },
+];
